85832d918879258dd35c3e6347a1f8251800d228
PriyaKondamuru <hkondamu@amd.com>
2025-09-09 15:39:06 +0530

    progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
 import { useState } from "react";
-import Password from "./components/Password";
+import Progressbar from "./components/Progressbar";
 export default function App() {
  
   return (
     <div className="app">
-      <Password/>
+      <Progressbar/>
     </div>
   );
 }
